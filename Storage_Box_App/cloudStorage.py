1ee8ad9b8715f5408753cfff466ffae40e26d03f
FM190 <86798311+FM190@users.noreply.github.com>
2022-02-19 19:46:19 -0500

    Add files via upload

---
+++ Storage_Box_App/cloudStorage.py
@@ -0,0 +1,27 @@
+
+import dropbox
+
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        """upload a file to Dropbox using API v2
+        """
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
+
+def main():
+    access_token = 'sl.BCWCoi6oF_GthH9Jg-kUJaIJsNHYLhDQwDiEHbKUoxdfKb9ePYtbf-430sFSZuWphAdX8au3UXkyW7ppsY_dmvUoIY2VmCRK84t0nLF9XCbODW90uPFOFzKuIcaha309HIN9gZgN0cMe'
+    transferData = TransferData(access_token)
+
+    file_from = 'test.txt'
+    file_to = '/test_dropbox/test.txt'  # The full path to upload the file to, including the file name
+
+    # API v2
+    transferData.upload_file(file_from, file_to)
+
+if __name__ == '__main__':
+    main()
